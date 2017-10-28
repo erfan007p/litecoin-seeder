32197f610b562ba55bfca6096d63429b140e7062
Aliyul Erfan <erfan007p@gmail.com>
2017-10-28 19:44:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -400,8 +400,8 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.flurbo.xyz", "seed.prenges.rocks", ""};
-static const string testnet_seeds[] = {"testnet-seed.litecointools.com", "seed-b.litecoin.loshan.co.uk", "testnet-seed.ltc.xurious.com", "dnsseed-testnet.thrasher.io", ""};
+static const string mainnet_seeds[] = {"seed.prenges.rocks", ""};
+static const string testnet_seeds[] = {"seed.prenges.rocks", ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
